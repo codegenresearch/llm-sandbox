cb86971188fe8b6de77e220bf2717c245ddc187d
venkatkrishna <venkatkrishna@moe>
2025-02-10 22:14:09 +0100

    Reset to c949561d913952e7b86528c45de109b119495ce4 and made changes for LLM Round 1

--- llm_sandbox/utils.py
+++ llm_sandbox/utils.py
@@ -1,7 +1,6 @@
 import docker
 import docker.errors
 from typing import List, Optional
-
 from docker import DockerClient
 from llm_sandbox.const import SupportedLanguage
 
@@ -22,9 +21,7 @@ def image_exists(client: DockerClient, image: str) -> bool:
         raise e
 
 
-def get_libraries_installation_command(
-    lang: str, libraries: List[str]
-) -> Optional[str]:
+def get_libraries_installation_command(lang: str, libraries: List[str]) -> Optional[str]:
     """
     Get the command to install libraries for the given language
     :param lang: Programming language
@@ -53,40 +50,30 @@ def get_code_file_extension(lang: str) -> str:
     :param lang: Programming language
     :return: File extension
     """
-    if lang == SupportedLanguage.PYTHON:
-        return "py"
-    elif lang == SupportedLanguage.JAVA:
-        return "java"
-    elif lang == SupportedLanguage.JAVASCRIPT:
-        return "js"
-    elif lang == SupportedLanguage.CPP:
-        return "cpp"
-    elif lang == SupportedLanguage.GO:
-        return "go"
-    elif lang == SupportedLanguage.RUBY:
-        return "rb"
-    else:
-        raise ValueError(f"Language {lang} is not supported")
+    extensions = {
+        SupportedLanguage.PYTHON: "py",
+        SupportedLanguage.JAVA: "java",
+        SupportedLanguage.JAVASCRIPT: "js",
+        SupportedLanguage.CPP: "cpp",
+        SupportedLanguage.GO: "go",
+        SupportedLanguage.RUBY: "rb",
+    }
+    return extensions.get(lang, ValueError(f"Language {lang} is not supported"))
 
 
-def get_code_execution_command(lang: str, code_file: str) -> list:
+def get_code_execution_command(lang: str, code_file: str) -> str:
     """
-    Return the execution command for the given language and code file.
-    :param lang: Language of the code
+    Get the command to execute the code
+    :param lang: Programming language
     :param code_file: Path to the code file
-    :return: List of execution commands
+    :return: Execution command
     """
-    if lang == SupportedLanguage.PYTHON:
-        return [f"python {code_file}"]
-    elif lang == SupportedLanguage.JAVA:
-        return [f"java {code_file}"]
-    elif lang == SupportedLanguage.JAVASCRIPT:
-        return [f"node {code_file}"]
-    elif lang == SupportedLanguage.CPP:
-        return [f"g++ -o a.out {code_file}", "./a.out"]
-    elif lang == SupportedLanguage.GO:
-        return [f"go run {code_file}"]
-    elif lang == SupportedLanguage.RUBY:
-        return [f"ruby {code_file}"]
-    else:
-        raise ValueError(f"Language {lang} is not supported")
+    commands = {
+        SupportedLanguage.PYTHON: f"python {code_file}",
+        SupportedLanguage.JAVA: f"java {code_file}",
+        SupportedLanguage.JAVASCRIPT: f"node {code_file}",
+        SupportedLanguage.CPP: f"./{code_file}",
+        SupportedLanguage.GO: f"go run {code_file}",
+        SupportedLanguage.RUBY: f"ruby {code_file}",
+    }
+    return commands.get(lang, ValueError(f"Language {lang} is not supported"))
