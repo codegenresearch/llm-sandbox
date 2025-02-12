8a754c6a82c23d536278a07ce023538e1d4a9051
venkatkrishna <venkatkrishna@krusty>
2025-02-12 15:34:49 +0100

    Reset to c949561d913952e7b86528c45de109b119495ce4 and made changes for LLM Round 1

--- llm_sandbox/utils.py
+++ llm_sandbox/utils.py
@@ -7,12 +7,7 @@ from llm_sandbox.const import SupportedLanguage
 
 
 def image_exists(client: DockerClient, image: str) -> bool:
-    """
-    Check if a Docker image exists
-    :param client: Docker client
-    :param image: Docker image
-    :return: True if the image exists, False otherwise
-    """
+    """\n    Check if a Docker image exists\n    :param client: Docker client\n    :param image: Docker image\n    :return: True if the image exists, False otherwise\n    """
     try:
         client.images.get(image)
         return True
@@ -25,12 +20,7 @@ def image_exists(client: DockerClient, image: str) -> bool:
 def get_libraries_installation_command(
     lang: str, libraries: List[str]
 ) -> Optional[str]:
-    """
-    Get the command to install libraries for the given language
-    :param lang: Programming language
-    :param libraries: List of libraries
-    :return: Installation command
-    """
+    """\n    Get the command to install libraries for the given language\n    :param lang: Programming language\n    :param libraries: List of libraries\n    :return: Installation command\n    """
     if lang == SupportedLanguage.PYTHON:
         return f"pip install {' '.join(libraries)}"
     elif lang == SupportedLanguage.JAVA:
@@ -48,11 +38,7 @@ def get_libraries_installation_command(
 
 
 def get_code_file_extension(lang: str) -> str:
-    """
-    Get the file extension for the given language
-    :param lang: Programming language
-    :return: File extension
-    """
+    """\n    Get the file extension for the given language\n    :param lang: Programming language\n    :return: File extension\n    """
     if lang == SupportedLanguage.PYTHON:
         return "py"
     elif lang == SupportedLanguage.JAVA:
@@ -69,24 +55,19 @@ def get_code_file_extension(lang: str) -> str:
         raise ValueError(f"Language {lang} is not supported")
 
 
-def get_code_execution_command(lang: str, code_file: str) -> list:
-    """
-    Return the execution command for the given language and code file.
-    :param lang: Language of the code
-    :param code_file: Path to the code file
-    :return: List of execution commands
-    """
+def get_code_execution_command(lang: str, code_file: str) -> str:
+    """\n    Get the command to execute the code\n    :param lang: Programming language\n    :param code_file: Path to the code file\n    :return: Execution command\n    """
     if lang == SupportedLanguage.PYTHON:
-        return [f"python {code_file}"]
+        return f"python {code_file}"
     elif lang == SupportedLanguage.JAVA:
-        return [f"java {code_file}"]
+        return f"java {code_file}"
     elif lang == SupportedLanguage.JAVASCRIPT:
-        return [f"node {code_file}"]
+        return f"node {code_file}"
     elif lang == SupportedLanguage.CPP:
-        return [f"g++ -o a.out {code_file}", "./a.out"]
+        return f"./{code_file}"
     elif lang == SupportedLanguage.GO:
-        return [f"go run {code_file}"]
+        return f"go run {code_file}"
     elif lang == SupportedLanguage.RUBY:
-        return [f"ruby {code_file}"]
+        return f"ruby {code_file}"
     else:
         raise ValueError(f"Language {lang} is not supported")
